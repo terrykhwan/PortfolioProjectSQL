7d6995d4f922cd5b62b27a503b7d34ea41baa188
terrykhwan <140210167+terrykhwan@users.noreply.github.com>
2023-10-09 01:18:15 +0400

    murdermysterycase-query.sql
    OVERVIEW:
    
    On the 15th of January 2018, a crime took place in SQL City and help is needed by the detective to solve this case. A crime scene report was given to me but I somehow lost it.  I vaguely remember certain details like the date and the city as previously mentioned.  To figure out the culprit, I started by getting information and the corresponding report for the police department database.
    
    All analysis of crimes committed on the 15th of January in SQL city from the police department database is highlighted in this report.
    
     Below are the steps taken to figure out who committed the crime

--- murdermysterycase-query.sql
+++ murdermysterycase-query.sql
@@ -1,18 +1,27 @@
+/* All analysis of crimes committed on the 15th of January in SQL city 
+from the police department database is highlighted in this report. */
+
+--filtering the table and identifying that the crime was a murder--
 SELECT *
 FROM crime_scene_report
 WHERE type = 'murder' AND date = 20180115 AND city ='SQL City';
 
+/* using the address in the result, "Northwestern Dr", of the first witness to, 
+identify other information (name and id) */
+	
 SELECT name, id, address_street_name
 FROM person
 WHERE address_street_name = 'Northwestern Dr'
 ORDER BY address_number DESC
 LIMIT 1;
 
-
+--Querying the Person table by filtering names, using what part of it as well as the address name.--
 SELECT name, id, address_street_name
 FROM person
 WHERE name LIKE '%Annabel%' AND address_street_name = 'Franklin Ave';
 
+/* Joining both the person table and interview table 
+to retrieve information(the details of each witness) by leveraging the relationships between them. */ 
 
 SELECT person.id, person.name, interview.transcript
 FROM person
@@ -20,7 +29,7 @@ JOIN interview
 ON person.id = interview.person_id
 WHERE person.id = 16371 OR person.id = 14887;
 
-
+--Querying to check the get_fit_now_member table and filtering the table based on the first witness--
 SELECT *
 FROM get_fit_now_member
 WHERE get_fit_now_member.id LIKE '%48z%' AND membership_status = 'gold';
@@ -30,18 +39,22 @@ SELECT *
 FROM get_fit_now_check_in
 WHERE check_in_date ='20180109' AND membership_id IN ( '48Z7A', '48Z55');
 
+
+/*using the schema diagram to join the person table (primary key id) and driver license table (foreign key id)
+and filtering with their person_id */
+
 SELECT person.name, drivers_license.gender, person.ssn, drivers_license.plate_number, drivers_license.car_make, person.address_street_name, person.address_number 
 FROM person
 JOIN drivers_license
 ON drivers_license.id = person.license_id
 WHERE person.id IN (28819, 67318);
 
-
+--Querying transcript from the interview table--
 SELECT *
 FROM interview
 WHERE person_id = '67318';
 
-
+--Using the information given in transcript to identify suspect--
 SELECT person.name, person. address_number, person. address_street_name, person.ssn
 FROm person
 JOIN drivers_license
@@ -56,8 +69,8 @@ WHERE facebook_event_checkin.event_name is 'SQL Symphony Concert'
 GROUP by person.name
 HAVING count(*) == 3;
 
-
+--solving the case and confirming solution-- 
 INSERT INTO solution(user, value)
 VALUES (1, 'Miranda Priestly');
 SELECT *
-FROm solution;
+FROM solution;
